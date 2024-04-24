8007bc23f971e50a9b45e7c33e83c06532920e1f
AokiKazunori <166583941+35195AokiKazunori@users.noreply.github.com>
2024-04-24 10:53:57 +0900

    売上集計データ新規作成

---
+++ Chapter02/SalesCounter/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesCounter {
+    internal class Program {
+        static void Main(string[] args) {
+            //戻り値を受け止める
+            List<Sale> sales = ReadSales(@"C:Users\infosys\source\repos\OOP2024\Chapter02\SalesCounter\data\bin\Debug\data");
+
+            //戻り値のコレクションを一件ずつ出力する
+            foreach (Sale sale in sales) {
+                Console.WriteLine(sale.ShopName + " " + sale.ProductCategory + " " + sale.Amount);
+
+                Console.WriteLine("店名:{0} カテゴリー:{1} 売上:{2}", sale.ShopName, sale.ProductCategory, sale.Amount);
+
+                Console.WriteLine($"店名:{sale.ShopName} カテゴリー:{sale.ProductCategory} 売上:{sale.Amount}");
+            }
+        }
+
+        //売上データを読み込み、Saleオブジェクトのリストを返す
+        static List<Sale> ReadSales(string filePath) {
+            List<Sale> sales = new List<Sale>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines) {
+                string[] items = line.Split(',');
+                Sale sale = new Sale {
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2]),
+                };
+                sales.Add(sale);
+            }
+            return sales;
+        }
+
+    }
+}
