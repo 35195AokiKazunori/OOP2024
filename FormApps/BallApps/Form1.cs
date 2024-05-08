d37aa3e1f1d9052eccad3211a8505d7fd8b023ff
AokiKazunori <166583941+35195AokiKazunori@users.noreply.github.com>
2024-05-08 16:00:04 +0900

    ボールを複数個同時に移動

--- FormApps/BallApps/Form1.cs
+++ FormApps/BallApps/Form1.cs
@@ -1,7 +1,11 @@
 namespace BallApps {
     public partial class Form1 : Form {
-        Obj obj;
-        PictureBox pb;
+        //Obj obj;
+        //PictureBox pb;
+        
+        //Listコレクション
+        private List<Obj> balls = new List<Obj>();
+        private List<PictureBox> pbs = new List<PictureBox>();
 
         //コンストラクタ
         public Form1() {
@@ -15,26 +19,35 @@ namespace BallApps {
         }
 
         private void timer1_Tick(object sender, EventArgs e) {
-            obj.Move();
-            pb.Location = new Point((int)obj.PosX, (int)obj.PosY);
+            //obj.Move();
+            //pb.Location = new Point((int)obj.PosX, (int)obj.PosY);
+
+            for(int i = 0; i < balls.Count; i++) {
+                balls[i].Move();
+                pbs[i].Location = new Point((int)balls[i].PosX, (int)balls[i].PosY);
+            }
+
         }
 
         private void timer2_Tick(object sender, EventArgs e) {
-            obj.Move();
-            pb.Location = new Point((int)obj.PosX, (int)obj.PosY);
+            //obj.Move();
+            //pb.Location = new Point((int)obj.PosX, (int)obj.PosY);
+            for(int i = 0; i < balls.Count; i++) {
+                balls[i].Move();
+                pbs[i].Location = new Point((int)balls[i].PosX, (int)balls[i].PosY);
+            }
         }
 
         private void Form1_MouseClick(object sender, MouseEventArgs e) {
-
-            pb = new PictureBox();   //画像を表示するコントロール
-            pb.Size = new Size(50, 50);
+            PictureBox pb = new PictureBox();   //画像を表示するコントロール
+            Obj obj = null;
 
             if (e.Button == MouseButtons.Left) {
                 ; obj = new SoccerBall(0, 0);
                 pb.Size = new Size(50, 50);
                 timer1.Start();
 
-            }else if(e.Button == MouseButtons.Right) {
+            } else if (e.Button == MouseButtons.Right) {
                 ; obj = new TennisBall(e.X - 25, e.Y - 25);
                 pb.Size = new Size(25, 25);
                 timer2.Start();
@@ -43,6 +56,9 @@ namespace BallApps {
             pb.Location = new Point((int)obj.PosX, (int)obj.PosY);
             pb.SizeMode = PictureBoxSizeMode.StretchImage;
             pb.Parent = this;
+
+            balls.Add(obj);
+            pbs.Add(pb);
         }
     }
 }
