767d47a9bf9c38cc6b5f06a9c7a393390ae1031a
AokiKazunori <166583941+35195AokiKazunori@users.noreply.github.com>
2024-05-15 11:50:54 +0900

    合計と平均 ラムダ

---
+++ Chapter03/Section01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var numbers = new[] { 4, 5, 6, 1, 4, 1, 4, 5, 7, 2, 6, 8, 2, 9 };
+
+            //int count = numbers.Count(n => n % 2 == 0);
+            double num = numbers.Where(n => n > 5).Average();
+            int total = numbers.Where(n => n > 5).Sum();
+            Console.WriteLine(num);
+            Console.WriteLine(total);
+        }
+    }
+}
