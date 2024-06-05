db1b80834375344bced39d097a9f3ce2cb8de791
AokiKazunori <166583941+35195AokiKazunori@users.noreply.github.com>
2024-06-05 13:11:56 +0900

    第6章　演習問題

---
+++ chapter06/Section01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            //Priceの降順出力
+            var books = Books.GetBooks().OrderBy(x => x.Title).ToList();
+            foreach (var book in books) {
+                Console.WriteLine(book.Title + ":" + book.Price);
+            }
+
+            //ページ数が最も多いbookを出力
+            //var page = Books.GetBooks().Max(x => x.Pages);
+            //var bookTitle = Books.GetBooks()
+            //                     .FirstOrDefault(b => b.Pages == page);
+            //Console.WriteLine(bookTitle.Title);
+        }
+    }
+}
