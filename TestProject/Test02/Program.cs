d0179885616df2f9a4c6d5993be1324e2675df65
AokiKazunori <166583941+35195AokiKazunori@users.noreply.github.com>
2024-06-18 12:10:26 +0900

    設問2_提出

--- TestProject/Test02/Program.cs
+++ TestProject/Test02/Program.cs
@@ -67,30 +67,29 @@ namespace Test02 {
         //問題１　合計値を表示
         //　　　　出力結果【618】
         private static void Exercise01(List<int> numbers) {
-
-
-
+            var sum = numbers.Sum();
+            Console.WriteLine(sum);
         }
 
         //問題２　偶数の最大値を表示
         //　　　　出力結果【94】
         private static void Exercise02(List<int> numbers) {
-
+            var max = numbers.Max();
+            Console.WriteLine(max);
 
 
         }
         //問題３　奇数のみを降順に並べて表示（遅延実行とする）
         //　　　　出力結果【91 87 53 35 31 17】
         private static void Exercise03(List<int> numbers) {
-
-
+            
 
         }
 
         //問題４　10以上50以下の数字のみを表示（即時実行でも可とする）
         //　　　　出力結果【12 14 20 40 35 31 17 48】
         private static void Exercise04(List<int> numbers) {
-
+            
 
 
         }
@@ -98,7 +97,8 @@ namespace Test02 {
         //問題５　平均年齢を表示
         //　　　　出力結果【30.2才】
         private static void Exercise05(List<Person> persons) {
-            
+            var average = persons.Average(x => x.Age);
+            Console.WriteLine(average);
 
 
         }
@@ -106,7 +106,7 @@ namespace Test02 {
         //問題６　身長170cm以上の体重平均を表示
         //　　　　出力結果【72.25kg】
         private static void Exercise06(List<Person> persons) {
-
+            
 
 
         }
@@ -125,7 +125,7 @@ namespace Test02 {
         //　　　　出力結果【山田隆司
         //　　　　　　　　　片山伸介】
         private static void Exercise08(List<Person> persons) {
-
+            
 
 
         }
